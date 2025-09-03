19f5b0f2aff23d6ff93b2a25d093924555f15617
Germán Jurado <juniorjurado2004@gmail.com>
2025-09-03 15:31:59 -0500

    Configuración inicial del proyecto

---
+++ s3-portafolio/providers.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = ">= 1.6.0"
+
+  required_providers {
+	aws = {
+		source = "hashicorp/aws"
+		version = "~> 5.0"
+	}
+  }
+}
+
+provider "aws" {
+  region = aws.aws_region
+}
+
+# Provider para us-east-1 (El ACM debe crearse en us-east-1 para CloudFront)
+provider "aws" {
+  alias = "us_east_1"
+  region = "us-east-1"
+}
---
+++ s3-portafolio/variables.tf
@@ -0,0 +1,25 @@
+variable "aws_region" {
+  type = string
+  default = "us-east-1"
+}
+
+variable "project_name" {
+  type = string
+  default = "portafolio"
+}
+
+variable "domain_name" {
+  type = string
+  description = "Dominio raíz (ej:GermanJurado.cloud)"
+}
+
+variable "subdomain" {
+  type = string
+  description = "Subdominio (ej: www). vacio para apex"
+  default = "www"
+}
+
+variable "hosted_zone_name" {
+  type = string
+  description = "Nombre de la Hosted Zone Pública (ej: GermanJurado.com.)"
+}
