fcaa5f31f799021e55178c8d7a1e26e844a17315
Germán Jurado <juniorjurado2004@gmail.com>
2025-09-26 13:47:11 -0500

    fix: format terraform files

--- pipeline-github-actions/main.tf
+++ pipeline-github-actions/main.tf
@@ -4,79 +4,79 @@ provider "aws" {
 
 # S3 Bucket (privado, usado solo por CloudFront)
 resource "aws_s3_bucket" "static_site" {
-	bucket = var.bucket_name
+  bucket = var.bucket_name
 
-	tags = {
-	  Name = "static-site"
-	  Environment = "dev"
-	  Owner = "Junior"
-	}
+  tags = {
+    Name        = "static-site"
+    Environment = "dev"
+    Owner       = "Junior"
+  }
 }
 
 # Bloquear acceso publico directo
 resource "aws_s3_bucket_public_access_block" "this" {
-  bucket = aws_s3_bucket.static_site.id
-  block_public_acls = true
-  block_public_policy = true
-  ignore_public_acls = true
+  bucket                  = aws_s3_bucket.static_site.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
   restrict_public_buckets = true
 }
 
 # Subir index.html
 resource "aws_s3_object" "index" {
-  bucket = aws_s3_bucket.static_site.bucket
-  key = "index.html"
-  source = "index.html"
+  bucket       = aws_s3_bucket.static_site.bucket
+  key          = "index.html"
+  source       = "index.html"
   content_type = "text/html"
-  etag = filemd5("index.html")
+  etag         = filemd5("index.html")
 }
 
 # OAC para CloudFront
 resource "aws_cloudfront_origin_access_control" "this" {
-  name = "${var.bucket_name}-oac"
-  description = "Control de acceso para CloudFront a S3"
+  name                              = "${var.bucket_name}-oac"
+  description                       = "Control de acceso para CloudFront a S3"
   origin_access_control_origin_type = "s3"
-  signing_behavior = "always"
-  signing_protocol = "sigv4"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
 }
 
 # CloudFront Distribution 
 resource "aws_cloudfront_distribution" "this" {
-  enabled = true
+  enabled             = true
   default_root_object = "index.html"
 
   origin {
-	domain_name = aws_s3_bucket.static_site.bucket_regional_domain_name
-	origin_id = aws_s3_bucket.static_site.id
-	origin_access_control_id = aws_cloudfront_origin_access_control.this.id
+    domain_name              = aws_s3_bucket.static_site.bucket_regional_domain_name
+    origin_id                = aws_s3_bucket.static_site.id
+    origin_access_control_id = aws_cloudfront_origin_access_control.this.id
   }
 
   default_cache_behavior {
-	target_origin_id = aws_s3_bucket.static_site.id
-	viewer_protocol_policy = "redirect-to-https"
-	allowed_methods = ["GET", "HEAD"]
-	cached_methods = ["GET", "HEAD"]
+    target_origin_id       = aws_s3_bucket.static_site.id
+    viewer_protocol_policy = "redirect-to-https"
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
 
-	forwarded_values {
-	  query_string = false
-	  cookies {
-		forward = "none"
-	  }
-	}
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
   }
 
   restrictions {
-	geo_restriction {
-	  restriction_type = "none"
-	}
+    geo_restriction {
+      restriction_type = "none"
+    }
   }
 
   viewer_certificate {
-	cloudfront_default_certificate = true
+    cloudfront_default_certificate = true
   }
 
   tags = {
-	Name = "static-site-cdn"
-	Environment = "dev"
+    Name        = "static-site-cdn"
+    Environment = "dev"
   }
 }
--- pipeline-github-actions/variables.tf
+++ pipeline-github-actions/variables.tf
@@ -1,9 +1,9 @@
 variable "aws_region" {
-  type = string
+  type    = string
   default = "us-east-1"
 }
 
 variable "bucket_name" {
-  type = string
+  type    = string
   default = "pipeline-github-actions"
 }
